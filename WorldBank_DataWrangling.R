085aa0c0c5c38b54e574ab7c6055e40344707688
egen97 <60612184+egen97@users.noreply.github.com>
2021-03-04 20:29:57 +0100

    Gjort ferdig GDP, må fikse populasjon

---
+++ WorldBank_DataWrangling.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(countrycode)
+#Cleane GDP og Population fra World Bank
+
+GDP <- read.csv("Data/gdpCAPITA.csv")
+Population <- read.csv("Data/population.csv")
+
+GDP <- rename(GDP, "Country" = "ï..Country.Name")
+Population <- rename(Population, "Country" = "ï..Country.Name")
+
+
+Non_Countries <- c( "Arab World", "Caribbean small states", 
+                    "Central Europe and the Baltics", "Channel Islands", "Early-demographic dividend", "East Asia & Pacific",
+                    "East Asia & Pacific (excluding high income)"," East Asia & Pacific (IDA & IBRD countries)", "Euro area", 
+                    "Europe & Central Asia", "Europe & Central Asia (excluding high income)"," Europe & Central Asia (IDA & IBRD countries)",
+                    "European Union", "Fragile and conflict affected situations", "Heavily indebted poor countries (HIPC)",
+                    "High income", "IBRD only", "IDA & IBRD total", "IDA blend", "IDA only", "IDA total", "Kosovo", 
+                    "Late-demographic dividend", "Latin America & Caribbean", "Latin America & Caribbean (excluding high income)",
+                    "Latin America & the Caribbean (IDA & IBRD countries)", "Least developed countries: UN classification", "Low & middle income", "Low income",
+                    "Lower middle income", "Middle East & North Africa", "Middle East & North Africa (excluding high income)",
+                    "Middle East & North Africa (IDA & IBRD countries)", "Middle income", "North America",
+                    "East Asia & Pacific (IDA & IBRD countries)", 
+                    "Europe & Central Asia (IDA & IBRD countries)", "Not classified", "OECD members", 
+                    "Other small states", "Pacific island small states", "Post-demographic dividend", 
+                    "Pre-demographic dividend", "Small states", 
+                    "South Asia", "South Asia (IDA & IBRD)", 
+                    "Sub-Saharan Africa", "Sub-Saharan Africa (excluding high income)", 
+                    "Sub-Saharan Africa (IDA & IBRD countries)", "Upper middle income", "World")
+
+
+GDP$Country <- ifelse(GDP$Country %in% Non_Countries, NA, GDP$Country)
+
+GDP$Country <- drop_na(GDP$Country)
+
+GDP_Cleaned <- GDP %>%
+  select(Country, starts_with("x")) %>%
+  filter(!is.na(Country)) %>%
+  pivot_longer(starts_with("x"), names_to = "year", values_to = "gdpPRcapita") %>%
+  mutate(year = str_remove(year, "X")) %>%
+  filter(year != "") %>%
+  mutate(year = trimws(year)) %>%
+  mutate(year = as.numeric(year))
+
+
+GDP_Cleaned$Country <- countrycode(GDP_Cleaned$Country, origin = "country.name", destination = "iso3n")
+
+
