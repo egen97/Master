a3f1ad631d188125debf079f535558620b671769
egen97 <60612184+egen97@users.noreply.github.com>
2021-03-04 11:07:41 +0100

    Fiksa missing

--- Polity_Five.R
+++ Polity_Five.R
@@ -1,10 +1,19 @@
 library(tidyverse)
 library(readxl)
+library(countrycode)
 #Laste inn polity V
 
 Polity <- read_excel("Data/p5v2018.xls")
 
 PolitySelected <- Polity %>%
   select(ccode, year, democ, polity) %>%
-  filter(year > 1979)
-saveRDS(PolitySelected, "politySelected.rds")
+  filter(year > 1979) %>%
+  mutate(across(everything(), ~ifelse(.x < -10, NA, .x)))
+
+PolitySelected$Country <- countrycode(PolitySelected$ccode, origin = "cown", destination = "iso3n")
+
+# Warning message:
+#   In countrycode(PolitySelected$ccode, origin = "cown", destination = "iso3n") :
+#   Some values were not matched unambiguously: 260, 265, 315, 342, 345, 347, 348, 364, 525, 529, 678, 680, 818
+
+saveRDS(PolitySelected, "Data/politySelected.rds")
