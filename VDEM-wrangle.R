04a2bac9fe2357b3d5f8b4110035141012dfd436
egen97 <60612184+egen97@users.noreply.github.com>
2021-03-24 19:12:34 +0100

    Lagt til noe fra VDEM, bør egt. fikse litt mer her men..prøver å legge til og kjøre, og se om det blir gøy (it prob will not)

---
+++ VDEM-wrangle.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+devtools::install_github("vdeminstitute/vdemdata")
+
+library(vdemdata)
+WVS <- readRDS("WVS.rds")
+Countries <- WVS$Country
+
+VDEM <- vdem
+
+
+VDEM$Country <- countrycode::countrycode(VDEM$country_name, origin = "country.name", destination = "iso3n")
+
+
+VDEM <- VDEM %>%
+  filter(year >= 1980) %>%
+  filter(!(Country %in% Countries))
+
+
+VDEM <- VDEM %>%
+  select(-starts_with("v3"))
+
+demoData <- VDEM %>%
+  select(v2x_polyarchy, v2x_libdem, v2x_egaldem, e_total_fuel_income_pc, e_total_resources_income_pc,
+         e_radio_n, e_miurbani, e_pefeliex, e_peinfmor, e_pelifeex, e_pematmor, v2xcl_acjst, v2x_corr,
+         v2xpe_exlecon, v2xpe_exlgender, v2xpe_exlpol, v2xpe_exlsocgr, v2x_accountability,v2caprotac,
+         v2cainsaut,v2caviol,v2mecenefm)
