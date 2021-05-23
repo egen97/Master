f3b8c4d3ff1e1ca05f84b07cd774ffac13710367
Eric G. E. Nilsen <60612184+egen97@users.noreply.github.com>
2021-05-23 14:47:28 +0200

    Lagt til AIC
    Tydeligvis vil ikke texreg hente ut goef fra zelig-modeller. Her har lagt til et enkelt gj.snitt, som ifølge internett er det beste, men litt usikker på den. Bør kanskje legge til noe annet goef også, df f.eks?

--- Tables.R
+++ Tables.R
@@ -1,19 +1,48 @@
 
+#Texreg klarer ikke hente ut AIC for some reason
+
+AIC_Extract <- function(Model){
+  
+  q <- lapply(from_zelig_model(Model), AIC)
+  
+  q <- as.data.frame(q)
+  
+  q <- rowMeans(q)
+  
+  return(q)
+  
+}
+
+
+Models <- list(UCDP_1, UCDP_1_FE, UCDP_2, UCDP_2_FE, UCDP_3, UCDP_3_FE,
+               UCDP_4, UCDP_4_FE, UCDP_5, UCDP_5_FE, UCDP_6, UCDP_6_FE)
+
+
+Models_UCDP <- list(UCDP_1, UCDP_2, UCDP_3, UCDP_1_FE, UCDP_2_FE, UCDP_3_FE,
+                    UCDP_4, UCDP_5, UCDP_4_FE, UCDP_5_FE, UCDP_6, UCDP_6_FE)
+
+
+aic_list_UCDP <- lapply(Models_UCDP, AIC_Extract)
+
+
 texreg::screenreg(l = list(UCDP_1, UCDP_2, UCDP_3), omit.coef = "(year)|(Country)",
-                   custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734)),
+                   custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734),
+                                          AIC = aic_list[c(1,3,5)]),
                    include.nobs = F,
                     custom.note = " %stars \n The time effect containts as well a squared and cubed effect",
                   custom.coef.names = c("(intercept)",
                                         "Value Score",
                                         "Time since conflict change",
                                         "Polity Score",
-                                        "Ln(GDP/cap)"))
+                                        "Ln(GDP/cap)")
+                  )
 
 
 # Tabel 1: Model 1, 2, 3 domestic variables
 
 texreg::texreg(l = list(UCDP_1, UCDP_2, UCDP_3), omit.coef = "(year)|(Country)",
-               custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734)),
+               custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734),
+                                      "AIC" = aic_list_UCDP[1:3]),
                include.nobs = F,
                custom.note = "%stars \n The time effect containts as well a squared and cubed effect",
                custom.coef.names = c("(intercept)",
@@ -46,9 +75,10 @@ texreg::screenreg(l = list(UCDP_1_FE, UCDP_2_FE, UCDP_3_FE), omit.coef = "(year)
 
 
 texreg::texreg( l = list(UCDP_1_FE, UCDP_2_FE, UCDP_3_FE), omit.coef = "(year)|(Country)",
-                custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734)),
+                custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734),
+                                       "AIC" = aic_list_UCDP[4:6]),
                 include.nobs = F,
-                custom.note = " %stars \n Fixed Effects on year & country",
+                custom.note = " %stars \n Fixed Effects on year and country",
                 custom.coef.names = c("(intercept)",
                                       "Value Score",
                                       "Time since conflict change",
@@ -78,8 +108,10 @@ texreg::screenreg(l = list(UCDP_4, UCDP_5),
                     "Borders: Sea",
                     "Borders: Land"
                   ),
-                  custom.gof.rows = list("Num. obs." = c(3734, 3734)),
+                  custom.gof.rows = list("Num. obs." = c(3734, 3734),
+                                         "AIC" = aic_list_UCDP[7:8]),
                   include.nobs = F,
+                  include.loglik = TRUE,
                   custom.model.names = c("Model 4", "Model 5")
                   )
 
@@ -97,7 +129,8 @@ texreg::texreg(l = list(UCDP_4, UCDP_5),
                     "Borders: Land"
                   ),
                custom.note = "%stars \n The time effect containts as well a squared and cubed effect",
-                  custom.gof.rows = list("Num. obs." = c(3734, 3734)),
+                  custom.gof.rows = list("Num. obs." = c(3734, 3734),
+                                         "AIC" = aic_list_UCDP[7:8]),
                   include.nobs = F,
                   custom.model.names = c("Model 4", "Model 5"),
                booktabs = TRUE,
@@ -125,7 +158,8 @@ texreg::screenreg(l = list(UCDP_4_FE, UCDP_5_FE),
                     "Borders: Sea",
                     "Borders: Land"
                   ),
-                  custom.gof.rows = list("Num. obs." = c(3734, 3734)),
+                  custom.gof.rows = list("Num. obs." = c(3734, 3734),
+                                         "AIC" = aic_list_UCDP[9:10]),
                   include.nobs = F,
                   custom.model.names = c("Model 4 FE", "Model 5 FE"), 
                   "%stars 
@@ -147,7 +181,8 @@ texreg::texreg(l = list(UCDP_4_FE, UCDP_5_FE),
                  "Borders: Land"
                ),
                custom.note = "%stars \n Fixed effects on country and year",
-               custom.gof.rows = list("Num. obs." = c(3734, 3734)),
+               custom.gof.rows = list("Num. obs." = c(3734, 3734),
+                                      "AIC" = aic_list_UCDP[9:10]),
                include.nobs = F,
                custom.model.names = c("Model 4 FE", "Model 5 FE"),
                booktabs = TRUE,
@@ -189,7 +224,8 @@ texreg::texreg(l = list(UCDP_6, UCDP_6_FE), omit.coef = c("Country|year"),
                  "CINC",
                  "Major Power"
                ),
-               custom.gof.rows = list("Num. obs" = c(3734, 3734)),
+               custom.gof.rows = list("Num. obs" = c(3734, 3734),
+                                      "AIC" = aic_list_UCDP[11:12]),
                include.nobs = F,
                custom.model.names = c("Model 6", "Model 6 FE"),
                booktabs = TRUE,
@@ -202,6 +238,12 @@ texreg::texreg(l = list(UCDP_6, UCDP_6_FE), omit.coef = c("Country|year"),
 
 ######## MID ###############
 
+#MID AIC
+
+Models_MID <- list(MID_1, MID_2, MID_3, MID_1_FE, MID_2_FE, MID_3_FE,
+                   MID_4, MID_5, MID_4_FE, MID_5_FE, MID_6, MID_6_FE)
+
+aic_list_MID <- lapply(Models_MID, AIC_Extract)
 
 
 
@@ -210,7 +252,8 @@ texreg::texreg(l = list(UCDP_6, UCDP_6_FE), omit.coef = c("Country|year"),
 # Tabel 1: Model 1, 2, 3 domestic variables
 
 texreg::texreg(l = list(MID_1, MID_2, MID_3), omit.coef = "(year)|(Country)",
-               custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734)),
+               custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734),
+                                      "AIC" = aic_list_MID[1:3]),
                include.nobs = F,
                custom.note = "%stars \n The time effect containts as well a squared and cubed effect",
                custom.coef.names = c("(intercept)",
@@ -234,7 +277,8 @@ texreg::texreg(l = list(MID_1, MID_2, MID_3), omit.coef = "(year)|(Country)",
 
 
 texreg::texreg( l = list(MID_1_FE, MID_2_FE, MID_3_FE), omit.coef = "(year)|(Country)",
-                custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734)),
+                custom.gof.rows = list("Num. obs." = c(3734, 3734, 3734),
+                                       "AIC" = aic_list_MID[4:6]),
                 include.nobs = F,
                 custom.note = " %stars \n Fixed Effects on year & country",
                 custom.coef.names = c("(intercept)",
@@ -270,7 +314,8 @@ texreg::texreg(l = list(MID_4, MID_5),
                  "Borders: Land"
                ),
                custom.note = "%stars \n The time effect containts as well a squared and cubed effect",
-               custom.gof.rows = list("Num. obs." = c(3734, 3734)),
+               custom.gof.rows = list("Num. obs." = c(3734, 3734),
+                                      "AIC" = aic_list_MID[7:8]),
                include.nobs = F,
                custom.model.names = c("Model 4", "Model 5"),
                booktabs = TRUE,
@@ -301,7 +346,8 @@ texreg::texreg(l = list(MID_4_FE, MID_5_FE),
                  "Borders: Land"
                ),
                custom.note = "%stars \n Fixed effects on country and year",
-               custom.gof.rows = list("Num. obs." = c(3734, 3734)),
+               custom.gof.rows = list("Num. obs." = c(3734, 3734),
+                                      "AIC" = aic_list_MID[9:10]),
                include.nobs = F,
                custom.model.names = c("Model 4 FE", "Model 5 FE"),
                booktabs = TRUE,
@@ -328,7 +374,8 @@ texreg::texreg(l = list(MID_6, MID_6_FE), omit.coef = c("Country|year"),
                  "CINC",
                  "Major Power"
                ),
-               custom.gof.rows = list("Num. obs" = c(3734, 3734)),
+               custom.gof.rows = list("Num. obs" = c(3734, 3734),
+                                      "AIC" = aic_list_MID[11:12]),
                include.nobs = F,
                custom.model.names = c("Model 6", "Model 6 FE"),
                booktabs = TRUE,
